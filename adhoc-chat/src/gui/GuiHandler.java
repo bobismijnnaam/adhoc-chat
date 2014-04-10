c8c2c7e0f15d9c480b3f1d438969ed52dec2bea6
rubenhaasjes@gmail.com <rubenhaasjes@gmail.com>
2014-04-10 14:18:36 +0000

    Cleaned sjit up

--- adhoc-chat/src/gui/GuiHandler.java
+++ adhoc-chat/src/gui/GuiHandler.java
@@ -10,7 +10,6 @@ import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.HashMap;
 
 import javax.swing.JButton;
@@ -18,15 +17,13 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.WindowConstants;
 
-import crypto.Crypto;
-
 import util.GradientList;
-
-import adhoc.AdhocSocket.AdhocListener;
 import adhoc.AdhocSocket;
+import adhoc.AdhocSocket.AdhocListener;
 import adhoc.Connection;
 import adhoc.Packet;
 import adhoc.ReliableSocket;
+import crypto.Crypto;
 
 public class GuiHandler implements java.awt.event.ActionListener, AdhocListener{
 	private static final byte CHAT_TYPE = 1;
@@ -36,11 +33,19 @@ public class GuiHandler implements java.awt.event.ActionListener, AdhocListener{
 	private JFrame frame;
 	private JPanel panel, mainScreenPanel;
 	private MainScreen mainScreen;
+	
+	// if in mainscreen
 	private boolean main = false;
+	
+	// userlist, adreslists, colorlist
 	private HashMap<Byte, String> users = new HashMap<Byte, String>();
 	private HashMap<String, Byte> addr = new HashMap<String, Byte>();
 	private HashMap<String, GradientList.Gradient> colors = new HashMap<String, GradientList.Gradient>();
+	
+	// socket
 	private ReliableSocket socket;
+	
+	// data format
 	private DateFormat df = new SimpleDateFormat("dd:MM:yy:HH:mm:ss");
 	
 	public GuiHandler() {
@@ -56,31 +61,23 @@ public class GuiHandler implements java.awt.event.ActionListener, AdhocListener{
 		loginGUI.addController(this);
 		panel = loginGUI.createPanel();
 		
+		// add the mainpanel
 		frame.add(panel);
 	    frame.setVisible(true);
 	    frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 	}
 	
-	/*
+	/**
 	 * Attempts to login, checks the username changes color to red if username
 	 * bad, else go to chat main screen.
 	 */
 	private void tryLogin(String username) {
-		// check username
+		// check username (longer than 3 characters only numbers and characters)
 		if (loginGUI.getUsername().matches("\\w{3,}+")) {
-			
-//			try {
-//				socket = new AdhocSocket(loginGUI.getUsername());
-//			} catch (IOException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
-//			socket.addListener(this);
 			try {
 				socket = new ReliableSocket(username, Crypto.INSTANCE.getMyKey());
 				socket.addListener(this);
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			
@@ -90,14 +87,12 @@ public class GuiHandler implements java.awt.event.ActionListener, AdhocListener{
 			frame.setSize(0, 0);
 			frame.setSize(800, 700);
 			main = true;
+			
+			// create the groupchat panel
 			mainScreen = new MainScreen(loginGUI.getUsername());
 			mainScreenPanel = mainScreen.returnPanel();
 			mainScreen.addChat("GroupChat", this);
 			mainScreen.changeChat("GroupChat");
-			//mainScreen.addChat("Willem", this);
-			//mainScreen.addChat("Bob", this);
-			//mainScreen.addChat("Michiel", this);
-			//mainScreen.addChat("Ruben", this);
 			frame.add(mainScreenPanel);
 			frame.setLocationRelativeTo(null);
 			frame.pack();
@@ -107,34 +102,47 @@ public class GuiHandler implements java.awt.event.ActionListener, AdhocListener{
 		}
 	}
 	
+	/**
+	 * Processes the message, gives the correct parameters to sendmessage
+	 */
+	private void processMessage(String group) {
+		// retrieve message from textfield in associated group
+		String message = mainScreen.getMessage(group);
+		if (!message.equals("") && message.trim().length() > 0 ) {
+			try {
+				// send message to group (or individual)
+				sendMessage(message, group);
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+			
+			Message newMessage;
+			long timestamp = System.currentTimeMillis();
+			Calendar cal = Calendar.getInstance();
+			cal.setTimeInMillis(timestamp);
+			String date = df.format(cal.getTime());
+			newMessage = mainScreen.addMessage(message, mainScreen.getUsername(), "#f22d2d", "#d10c0c", false, group, date);
+			frame.pack();
+			mainScreen.addSize(newMessage.getBounds().y, group);
+			frame.pack();
+			mainScreen.scrollDown(group);
+		}
+	}
+	
+	/**
+	 * Triggered when an action in the UI is performed
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		//System.out.println("Event triggered");
 		if (main && ((Component) e.getSource()).getName().contains("enter")) {
 			String[] messageParts = ((Component) e.getSource()).getName().split("enter");
-			String message = mainScreen.getMessage(messageParts[1]);
-			if (!message.equals("") && message.trim().length() > 0 ) {
-				// send chatmessage
-				try {
-					sendMessage(message, messageParts[1]);
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-				//UDPsocket.sendChatMessage(addr.get(messageParts[1]), 0, message);
-				Message newMessage;
-				long timestamp = System.currentTimeMillis();
-				Calendar cal = Calendar.getInstance();
-				cal.setTimeInMillis(timestamp);
-				String date = df.format(cal.getTime());
-				newMessage = mainScreen.addMessage(message, mainScreen.getUsername(), "#f22d2d", "#d10c0c", false, messageParts[1], date);
-				frame.pack();
-				mainScreen.addSize(newMessage.getBounds().y, messageParts[1]);
-				frame.pack();
-				mainScreen.scrollDown(messageParts[1]);
-			}
+			String group = messageParts[1];
+			
+			// process the message
+			processMessage(group);
 		} else {
 			if (((Component) e.getSource()).getName().equals("loginkey")) {
+				// try login
 				tryLogin(loginGUI.getUsername());
 			} else {
 				JButton source = (JButton) e.getSource();
@@ -143,26 +151,9 @@ public class GuiHandler implements java.awt.event.ActionListener, AdhocListener{
 					tryLogin(loginGUI.getUsername());
 				} else if (source.getName().contains("send")) {
 					String[] messageParts = ((Component) e.getSource()).getName().split("send");
-					String message = mainScreen.getMessage(messageParts[1]);
-					// send chatmessage
-					if (!message.equals("") && message.trim().length() > 0 ) {
-						try {
-							sendMessage(message, messageParts[1]);
-						} catch (IOException e1) {
-							// TODO Auto-generated catch block
-							e1.printStackTrace();
-						}
-						long timestamp = System.currentTimeMillis();
-						Calendar cal = Calendar.getInstance();
-						cal.setTimeInMillis(timestamp);
-						String date = df.format(cal.getTime());
-						//UDPsocket.sendChatMessage(addr.get(messageParts[1]), 0, message);
-						Message newMessage = mainScreen.addMessage(message, mainScreen.getUsername(), "#f22d2d", "#d10c0c", false, messageParts[1], date);
-						frame.pack();
-						mainScreen.addSize(newMessage.getBounds().y, messageParts[1]);
-						frame.pack();
-						mainScreen.scrollDown(messageParts[1]);
-					}
+					String group = messageParts[1];
+					// process the message
+					processMessage(group);
 				} else {
 					mainScreen.changeChat(source.getName());
 				}
@@ -170,9 +161,11 @@ public class GuiHandler implements java.awt.event.ActionListener, AdhocListener{
 		}
 	}
 
+	/**
+	 * Triggered when a new connection is esthablished, adds a new chat and user to list of neccesary
+	 */
 	@Override
 	public void newConnection(Connection connection) {
-		System.out.println("JEEJ EEN NIEUWE CONNECTIE");
 		mainScreen.addChat(connection.name, this);
 		users.put(connection.address, connection.name);
 		addr.put(connection.name, connection.address);
@@ -180,36 +173,42 @@ public class GuiHandler implements java.awt.event.ActionListener, AdhocListener{
 		int index = colors.size();
 		index = index % 6;
 		
+		// picks a color from the gradientcolor list and associates it with the username
 		GradientList.Gradient color = gradients.getGradient(index);
 		colors.put(connection.name, color);
 	}
 	
+	/**
+	 * Triggered when a connection is removed, removes user and chat from UI
+	 */
 	@Override
 	public void removedConnection(Connection connection) {
 		System.out.println("EEN CONNECTIE GING WEG");
 		mainScreen.removeUser(connection.name);
-		mainScreen.changeChat("GroupChat");
+		// because bob wants to reread messages from his lovers mainScreen.changeChat("GroupChat");
 	}
 	
+	/**
+	 * Triggered when a packet is received
+	 * @see adhoc.AdhocSocket.AdhocListener#onReceive(adhoc.Packet)
+	 */
 	@Override
 	public void onReceive(Packet packet) {
 		System.out.println("Receive not ack packet");
 		if (packet.getType() == (byte) 1) {
 			try {
 				System.out.println("Received a message");
-				
 				byte[] data = null;
-				
 				byte addr = packet.getSourceAddress();
 				
-				if(addr == AdhocSocket.MULTICAST_ADDRESS){
+				// if it's a broadcast
+				if (addr == AdhocSocket.MULTICAST_ADDRESS){
 					data = packet.getData();
-				}else{
+				} else {
 					data = Crypto.INSTANCE.decrypt(packet.getData());
 				}
 				
 				DataInputStream dataStream = new DataInputStream(new ByteArrayInputStream(data));
-				
 				long timestamp = dataStream.readLong();
 				
 				Calendar cal = Calendar.getInstance();
@@ -227,6 +226,7 @@ public class GuiHandler implements java.awt.event.ActionListener, AdhocListener{
 				frame.repaint();
 				mainScreen.scrollDown(username);
 				frame.pack();
+				
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
@@ -234,6 +234,12 @@ public class GuiHandler implements java.awt.event.ActionListener, AdhocListener{
 		}
 	}
 	
+	/**
+	 * Sends a message
+	 * @param inputMessage - message to send
+	 * @param username - username to send to
+	 * @throws IOException if something goes wrong
+	 */
 	private void sendMessage(String inputMessage, String username) throws IOException {
 		ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
 		DataOutputStream dataStream = new DataOutputStream(byteStream);
@@ -245,14 +251,16 @@ public class GuiHandler implements java.awt.event.ActionListener, AdhocListener{
 		
 		if(username.equals("GroupChat")){
 			socket.send(dest, CHAT_TYPE, byteStream.toByteArray());
-		}else{
+		} else{
 			socket.send(dest, CHAT_TYPE, Crypto.INSTANCE.encrypt(dest, byteStream.toByteArray()));
 		}
-		System.out.println("Send message");
 	}
 	
+	/**
+	 * starts the whole program!
+	 */
 	public static void main(String[] args) {
 		// guihandler
-		GuiHandler handler = new GuiHandler();
+		new GuiHandler();
 	}
 }

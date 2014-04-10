3b18e55629c5577b7b3fcaa036ec256d684b28b1
reycs <rubenhaasjes@gmail.com>
2014-04-10 13:18:07 +0200

    COLORS BITCH

--- adhoc-chat/src/util/GradientList.java
+++ adhoc-chat/src/util/GradientList.java
@@ -3,17 +3,13 @@ package util;
 
 public class GradientList {
 
-	private Gradient[] list = new Gradient[8];
+	private Gradient[] list = new Gradient[4];
 	
 	public GradientList() {
-		list[0] = new Gradient("#000000", "#000000");
-		list[1] = new Gradient("#000000", "#000000");
-		list[2] = new Gradient("#000000", "#000000");
-		list[3] = new Gradient("#000000", "#000000");
-		list[4] = new Gradient("#000000", "#000000");
-		list[5] = new Gradient("#000000", "#000000");
-		list[6] = new Gradient("#000000", "#000000");
-		list[7] = new Gradient("#000000", "#000000");
+		list[0] = new Gradient("#ff8415", "#ff8d22");
+		list[2] = new Gradient("#ff1fcd", "#cc12a3");
+		list[3] = new Gradient("#3210c3", "#2b119c");
+		list[1] = new Gradient("#525af4", "#434ae0");
 	}
 	
 	/**

0c775107b45166949a2af988d3125807ee32762b
rubenhaasjes@gmail.com <rubenhaasjes@gmail.com>
2014-04-10 16:01:51 +0000

    Small fixes

--- adhoc-chat/src/util/GradientList.java
+++ adhoc-chat/src/util/GradientList.java
@@ -1,10 +1,9 @@
 package util;
 
-
 public class GradientList {
 
 	private Gradient[] list = new Gradient[6];
-	
+
 	public GradientList() {
 		list[0] = new Gradient("#ff8415", "#ff8d22");
 		list[2] = new Gradient("#ff1fcd", "#cc12a3");
@@ -13,25 +12,24 @@ public class GradientList {
 		list[4] = new Gradient("#3de95a", "#31cb4b");
 		list[5] = new Gradient("#e5d12c", "#d7c424");
 	}
-	
+
 	/**
 	 * @returns an gradient color, associated with the index.
 	 */
 	public Gradient getGradient(int index) {
-		return list[0];
+		return list[index];
 	}
-	
+
 	/**
 	 * Contains two colors.
 	 */
 	public class Gradient {
 		public String color1;
 		public String color2;
-		
+
 		public Gradient(String inputColor1, String inputColor2) {
 			color1 = inputColor1;
 			color2 = inputColor2;
 		}
 	}
 }
-
